3f42807df412f3f310d901b1b242b00cf7af04c6
RussianLord <dj_volume@mail.ru>
2022-12-06 17:11:23 +0300

    Сделал полностью 3 задание

--- HW5.py
+++ HW5.py
@@ -14,33 +14,49 @@
 
 # 2. Создайте программу для игры в ""Крестики-нолики"".
 
+
+
+
 # 3. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 
-N = 'AAADDDDDAABBBCC'
+
 Mode = int(input('Нажми 1, чтобы выбрать сжатие, нажми 2, чтобвы выбрать восстановление... '))
-
-def Sjat(N):
-    K = 0
-    listN = []
-    for i in range(K, len(N)):
-        if N[K] != N[i]:
-            listN.append((i-K, N[i-1]))
-            K = i
-    listN.append((i-K+1, N[i-1]))
-    print(listN)
-
-    # change = int(input('Восстановить файл?...Да - 1... '))
-    # if change == 1:
-    #     Recov(N)
-
-Sjat(N)
-
-# def Recov(N):
+with open('Python_HW5/sjat.txt', 'r', encoding='utf-8') as R:
+    M = R.read()
+def Sjat(N):  
+        K = 0
+        strN = ""
+        for i in range(K, len(N)):
+            if N[K] != N[i]:
+                strN += ((f'{i-K} {N[i-1]},'))
+                K = i
+        strN += ((f'{i-K+1} {N[i-1]}'))
+        print(strN)
+        with open('Python_HW5/RECOV.txt', 'w', encoding='utf-8') as W:
+            P = W.write(strN) 
+           
 
 
-# if Mode == 1:
-#     Sjat(N)
-# else:
-#     Recov(N)
+with open('Python_HW5/RECOV.txt', 'r', encoding='utf-8') as R:
+    N = R.read().split(',')
+def Recov(N):
+    with open('Python_HW5/sjat.txt', 'w', encoding='utf-8') as W:
+        P = W.write('')
+    strN = ""
+    for i in range(len(N)): 
+        T = N[i].split()
+        number = int(T[0])
+        for j in range(number):
+            strN = T[1]
+            with open('Python_HW5/sjat.txt', 'a', encoding='utf-8') as W:
+                P = W.write(strN)
+            print(strN, end='')
+    
+
+
+if Mode == 1:
+    Sjat(M)
+else:
+    Recov(N)
